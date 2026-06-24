ca8f21d5eb4a484b3ad549d575efd4f089e6202d
Joseph Bucholtz <joseph.bucholtz@sjsu.edu>
2026-06-23 19:35:34 -0700

    figuring out design

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdint.h> 
+#include <vector>
 
 enum class OrderType {
     Market,
@@ -12,15 +13,6 @@ enum class Side {
     Sell,
 };
 
-class OrderBook {
-public:
-
-
-
-private:
-
-};
-
 class Order {
 public:
     Order(int orderId, OrderType orderType, Side side, uint32_t quantity, int price) {
@@ -31,6 +23,20 @@ public:
         m_price = { price };
     }
 
+    int getOrderId() { return m_orderId; }
+    OrderType getOrderType() { return m_orderType; }
+    Side getSide() { return m_side; }
+    uint32_t getQuantity() { return m_quantity; }
+    int getPrice() { return m_price; }
+    bool isFilled() { return m_quantity == 0; }
+
+    void fill(uint32_t quantity) {
+        if (quantity > m_quantity) {
+            return; 
+        }
+
+        m_quantity -= quantity;
+    }
     
 
 private:
@@ -42,6 +48,30 @@ private:
     
 
 };
+
+class OrderBook {
+public:
+    OrderBook () {}     
+    
+    void addOrder(Order order) {
+        Side side = order.getSide();
+
+        if (side == Side::Buy) {
+            m_bids.push_back(order);
+        }
+        else {
+            m_asks.push_back(order);
+        }
+    }
+
+    void removeOrder(int orderId);
+    void modifyOrder(int orderId);
+
+private:
+    std::vector<Order> m_bids {};
+    std::vector<Order> m_asks {}; 
+};
+
 
 int main () {
     return 0;
