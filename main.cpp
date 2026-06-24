9f75beae004a1ebbd16d7ea3455f5caab5fa1985
Joseph Bucholtz <joseph.bucholtz@sjsu.edu>
2026-06-23 18:51:04 -0700

    Orderbook/Order

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include <stdint.h> 
+
+enum class OrderType {
+    Market,
+    Limit,
+    StopLoss,
+};
+
+enum class Side {
+    Buy,
+    Sell,
+};
+
+class OrderBook {
+public:
+
+
+
+private:
+
+};
+
+class Order {
+public:
+    Order(int orderId, OrderType orderType, Side side, uint32_t quantity, int price) {
+        m_orderId = { orderId };
+        m_orderType = { orderType };
+        m_side = { side };
+        m_quantity = { quantity };
+        m_price = { price };
+    }
+
+    
+
+private:
+    int  m_orderId; 
+    OrderType m_orderType;
+    Side m_side;
+    uint32_t m_quantity;
+    int m_price;
+    
+
+};
 
 int main () {
-    std::cout << "Hello World!\n";
     return 0;
 }
