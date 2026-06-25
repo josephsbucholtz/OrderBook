97afed96a41f1cb5d46ab7853148330f2c048635
Joseph Bucholtz <joseph.bucholtz@sjsu.edu>
2026-06-24 17:23:09 -0700

    basic Client

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include <stdint.h> 
 #include <vector>
+#include <queue>
+#include <unordered_map>
+
+#include <stdint.h> 
 
 enum class OrderType {
-    Market,
-    Limit,
-    StopLoss,
+    MARKET,
+    LIMIT,
+    STOPLOSS,
 };
 
 enum class Side {
-    Buy,
-    Sell,
+    BUY,
+    SELL,
 };
 
 class Order {
@@ -23,6 +26,13 @@ public:
         m_price = { price };
     }
 
+    bool operator<(const Order& other) const {
+        return this->m_price < other.m_price;
+    }
+
+    bool operator>(const Order& other) const {
+        return this->m_price > other.m_price;
+    }
     int getOrderId() { return m_orderId; }
     OrderType getOrderType() { return m_orderType; }
     Side getSide() { return m_side; }
@@ -31,7 +41,8 @@ public:
     bool isFilled() { return m_quantity == 0; }
 
     void fill(uint32_t quantity) {
-        if (quantity > m_quantity) {
+        if (quantity > getQuantity()) {
+            std::cout << "Order cannot be filled\n";
             return; 
         }
 
@@ -52,27 +63,69 @@ private:
 class OrderBook {
 public:
     OrderBook () {}     
+
+    void placeOrder(Order order);
     
     void addOrder(Order order) {
         Side side = order.getSide();
 
-        if (side == Side::Buy) {
-            m_bids.push_back(order);
+        if (side == Side::BUY) {
+            m_bids.push(order);
         }
         else {
-            m_asks.push_back(order);
+            m_asks.push(order);
         }
     }
 
     void removeOrder(int orderId);
     void modifyOrder(int orderId);
 
+    std::priority_queue<Order> getBids() { return m_bids;}
+
 private:
-    std::vector<Order> m_bids {};
-    std::vector<Order> m_asks {}; 
+    std::priority_queue<Order> m_bids {}; //max-heap
+    std::priority_queue<Order, std::vector<Order>, std::greater<Order>> m_asks {}; //min-heap 
+    std::unordered_map<int, Order> orderMap ; //orderId to Order
+    
+
 };
 
 
+class Client {
+public:
+    Client(std::string name) :
+    m_name { name } 
+    {}
+
+    Client(std::string name, int balance) : 
+    m_name { name } , 
+    m_balance { balance }
+    {}
+
+    std::string getName() { return m_name; }; 
+    int getBalance() { return m_balance; };
+
+    void placeOrder(int amount);
+    void cancelOrder(const std::vector<Order>& orders);
+    void modifyOrder(const std::vector<Order>& orders);
+
+private:
+    std::string m_name;
+    int m_balance {}; 
+    std::vector<Order> m_orders {};
+};
+
 int main () {
+    Order order(0, OrderType::LIMIT, Side::BUY, 10, 100);
+
+    Client client("John", 50);
+    
+    std::cout << "CLient Name: " << client.getName() << "\n";
+    std::cout << "Balance: " << client.getBalance() << "\n";
+
+    OrderBook orderbook{};
+    orderbook.addOrder(order);
+    
+    std::cout << "Quantity: " << order.getQuantity() << "\n";
     return 0;
 }
